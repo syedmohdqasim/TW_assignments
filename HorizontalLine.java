b6976847141f8a5eff315ec1a671a6c0b7845f7e
syedmohdqasim <smdqsm@gmail.com>
2017-07-09 18:52:46 +0530

    HorizontalLineFinished

--- HorizontalLine.java
+++ HorizontalLine.java
@@ -1,7 +1,22 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by root on 9/7/17.
  */
 public class HorizontalLine {
+
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        for(int i=0;i<n;i++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+
+    }
+
 }
